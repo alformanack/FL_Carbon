711c17c7fe46cabb1725a29e29530983bbd2e09d
alformanack <57016718+alformanack@users.noreply.github.com>
2019-11-09 16:52:59 -0500

    using the same code all I get is NA for the averages. I'm thinking it is stuck at line 17 because the environment for rows says character (empty).

---
+++ avg c n.R
@@ -0,0 +1,33 @@
+setwd("C:/Users/Alicia/Desktop/FL")
+
+sitetreetemp<-read.csv("temperature_new_FL_SITETREE.csv", header=T, sep=",", stringsAsFactors = F)
+soils<-read.csv("FL_SOILS_LAB.csv", header=T, sep=",", stringsAsFactors = F)
+
+
+
+
+plot_id<-unique(soils$PLT_CN)
+
+
+for (i in 1:length(plot_id)){
+  test<-subset(soils,PLT_CN==plot_id [i])
+  layer1<-subset(test, LAYER_TYPE=="MIN_1")
+  layer2<-subset(test, LAYER_TYPE=="MIN_2")
+  Carbonavg<-(layer1$C_ORG_PCT+layer2$C_ORG_PCT)/2
+  rows<-row.names(sitetreetemp[which(sitetreetemp$PLT_CN==plot_id[i]),])
+  if(length(Carbonavg)>0){
+    sitetreetemp[rows,"avg_soil_c_pct"]<-Carbonavg
+  }
+}
+
+
+for (i in 1:length(plot_id)){
+  test<-subset(soils,PLT_CN==plot_id [i])
+  layer1<-subset(test, LAYER_TYPE=="MIN_1")
+  layer2<-subset(test, LAYER_TYPE=="MIN_2")
+  Nitrogenavg<-(layer1$N_TOTAL_PCT+layer2$N_TOTAL_PCT)/2
+  rows<-row.names(sitetreetemp[which(sitetreetemp$PLT_CN==plot_id[i]),])
+  if(length(Nitrogenavg)>0){
+    sitetreetemp[rows,"avg_soil_n_pct"]<-Nitrogenavg
+  }
+}
